588bcdb26a3a0adf351d666531fc23ea1b2d8188
Kroese <kroese@users.noreply.github.com>
2026-06-07 11:41:31 +0200

    Update entrypoint

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -23,51 +23,6 @@ echo ""
 # Update password for root
 printf 'root:%s\n' "$PASSWORD" | chpasswd
 
-# Get the capability bounding set
-CAP_BND=$(grep '^CapBnd:' /proc/$$/status | awk '{print $2}')
-CAP_BND=$(printf "%d" "0x${CAP_BND}")
-
-# Get the last capability number
-LAST_CAP=$(cat /proc/sys/kernel/cap_last_cap)
-
-# Calculate the maximum capability value
-MAX_CAP=$(((1 << (LAST_CAP + 1)) - 1))
-
-# Check if container is privileged
-if [ "${CAP_BND}" -ne "${MAX_CAP}" ]; then
-  warn "please start the container with the --privileged flag!"
-  #[[ "${DEBUG:-}" != [Yy1]* ]] && exit 14
-fi
-
-# Check if /dev/fuse is available
-if [ ! -c /dev/fuse ]; then
-  warn "could not access /dev/fuse, make sure this kernel module is loaded!"
-  #[[ "${DEBUG:-}" != [Yy1]* ]] && exit 16
-fi
-
-# Check KVM support
-KVM_ERR=""
-
-if [ ! -e /dev/kvm ]; then
-  KVM_ERR="(/dev/kvm is missing)"
-else
-  if ! sh -c 'echo -n > /dev/kvm' &> /dev/null; then
-    KVM_ERR="(/dev/kvm is unwriteable)"
-  else
-    if [ "$(uname -m)" = "x86_64" ]; then
-      flags=$(sed -ne '/^flags/s/^.*: //p' /proc/cpuinfo)
-      if ! grep -qw "vmx\|svm" <<< "$flags"; then
-        KVM_ERR="(not enabled in BIOS)"
-      fi
-    fi
-  fi
-fi
-
-if [ -n "$KVM_ERR" ]; then
-  warn "KVM acceleration is not available $KVM_ERR, see the FAQ for possible causes."
-  #[[ "${DEBUG:-}" != [Yy1]* ]] && exit 19
-fi
-
 # Fix permissions
 
 dir="/etc/proxmox-datacenter-manager"
